04bf8f0c0a0c7cb506f08e9cbf473ca87d72d91e
3D_MAMA <34866314+arvitus@users.noreply.github.com>
2026-04-12 20:57:54 +0200

    add custom translation key to customize the kick message, when a reason is given

--- src/main/java/de/arvitus/servermessages/mixin/context/commands/KickCommand.java
+++ src/main/java/de/arvitus/servermessages/mixin/context/commands/KickCommand.java
@@ -1,63 +1,64 @@
 package de.arvitus.servermessages.mixin.context.commands;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mojang.brigadier.context.CommandContext;
+import de.arvitus.servermessages.ServerMessages;
 import eu.pb4.placeholders.api.ServerPlaceholderContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import org.objectweb.asm.Opcodes;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Collection;
-
 import static de.arvitus.servermessages.ServerMessages.CONTEXT_STORE;
 
 @Mixin(net.minecraft.server.commands.KickCommand.class)
 public abstract class KickCommand {
     @Unique
-    private static boolean withoutReason = false;
+    private static boolean hasReason = false;
 
     @Inject(method = "lambda$register$0", at = @At("HEAD"))
-    private static void setType(CommandContext<CommandSourceStack> context, CallbackInfoReturnable<Integer> cir) {
-        withoutReason = true;
+    private static void setNoReason(CommandContext<CommandSourceStack> c, CallbackInfoReturnable<Integer> cir) {
+        hasReason = false;
     }
 
-    @Inject(
+    @Inject(method = "lambda$register$1", at = @At("HEAD"))
+    private static void setReason(CommandContext<CommandSourceStack> c, CallbackInfoReturnable<Integer> cir) {
+        hasReason = true;
+    }
+
+    @WrapOperation(
         method = "kickPlayers",
         at = @At(
-            value = "FIELD",
-            target = "Lnet/minecraft/server/level/ServerPlayer;" +
-                     "connection:Lnet/minecraft/server/network/ServerGamePacketListenerImpl;",
-            opcode = Opcodes.GETFIELD)
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;disconnect" +
+                     "(Lnet/minecraft/network/chat/Component;)V"
+        )
     )
     private static void setKickContext(
-        CommandSourceStack source,
-        Collection<ServerPlayer> targets,
-        Component reason,
-        CallbackInfoReturnable<Integer> cir,
+        ServerGamePacketListenerImpl instance,
+        Component component,
+        Operation<Void> original,
         @Local ServerPlayer player,
         @Local(argsOnly = true) LocalRef<Component> reasonRef
     ) {
         ServerPlaceholderContext context = ServerPlaceholderContext.of(player);
         CONTEXT_STORE.put("commands.kick.success", context);
-        if (!withoutReason) return;
-        CONTEXT_STORE.put("multiplayer.disconnect.kicked", context);
-        reasonRef.set(Component.translatable("multiplayer.disconnect.kicked"));
-    }
 
-    @Inject(method = "kickPlayers", at = @At("RETURN"))
-    private static void resetType(
-        CommandSourceStack source,
-        Collection<ServerPlayer> targets,
-        Component reason,
-        CallbackInfoReturnable<Integer> cir
-    ) {
-        withoutReason = false;
+        CONTEXT_STORE.put("multiplayer.disconnect.kicked", context);
+        if (!hasReason) {
+            original.call(instance, Component.translatable("multiplayer.disconnect.kicked"));
+            ServerMessages.withDisabled(() -> reasonRef.set(Component.translatable("multiplayer.disconnect.kicked")));
+        } else original.call(
+            instance,
+            Component.translatableWithFallback("multiplayer.disconnect.kicked.reason", "%s", component)
+        );
     }
 }

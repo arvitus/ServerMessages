a5cd056903feeb592b49530e3088069071070d7b
3D_MAMA <34866314+arvitus@users.noreply.github.com>
2026-05-08 20:03:29 +0200

    fix mixin errors

--- src/main/java/de/arvitus/servermessages/mixin/context/commands/DeOpCommandsMixin.java
+++ src/main/java/de/arvitus/servermessages/mixin/context/commands/DeOpCommandsMixin.java
@@ -1,28 +1,44 @@
 package de.arvitus.servermessages.mixin.context.commands;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import de.arvitus.servermessages.ServerMessages;
 import eu.pb4.placeholders.api.ServerPlaceholderContext;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.commands.DeOpCommands;
 import net.minecraft.server.players.NameAndId;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.Collection;
+import java.util.function.Supplier;
 
 @Mixin(DeOpCommands.class)
 public class DeOpCommandsMixin {
-    @WrapMethod(method = "lambda$deopPlayers$0")
-    private static Component setFeedbackContext(
-        Collection<NameAndId> players,
-        Operation<Component> original,
+    @WrapOperation(method = "deopPlayers",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/commands/CommandSourceStack;sendSuccess(Ljava/util/function/Supplier;Z)V"
+        )
+    )
+    private static void setFeedbackContext(
+        CommandSourceStack instance,
+        Supplier<Component> messageSupplier,
+        boolean broadcast,
+        Operation<Void> original,
         @Local NameAndId nameAndId
     ) {
-        if (ServerMessages.SERVER == null) return original.call(players);
-        var player = ServerMessages.SERVER.getPlayerList().getPlayer(nameAndId.id());
-        if (player == null) return original.call(players);
-        return ServerMessages.withContext(ServerPlaceholderContext.of(player), () -> original.call(players));
+        if (ServerMessages.SERVER != null) {
+            var player = ServerMessages.SERVER.getPlayerList().getPlayer(nameAndId.id());
+            if (player != null) {
+                ServerMessages.withContext(
+                    ServerPlaceholderContext.of(player),
+                    () -> original.call(instance, messageSupplier, broadcast)
+                );
+                return;
+            }
+        }
+        original.call(instance, messageSupplier, broadcast);
     }
 }
